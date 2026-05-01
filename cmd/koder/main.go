f7a411f305b900d61e80fc07fea927accd3a805b
Lars Karlslund <lars@karlslund.dk>
2026-05-01 04:44:10 +0200

    Move CLI wiring into cmd koder

--- cmd/koder/main.go
+++ cmd/koder/main.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"fmt"
 	"os"
-
-	"github.com/lkarlslund/koder/internal/app"
 )
 
 func main() {
-	if err := app.NewRootCommand().ExecuteContext(context.Background()); err != nil {
+	if err := NewRootCommand().ExecuteContext(context.Background()); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
